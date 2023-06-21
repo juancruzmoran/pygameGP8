232b6b85b55253327765e518f0f057a4f5eeab57
SofiPavesi <99208155+SofiPavesi@users.noreply.github.com>
2023-06-21 13:56:29 -0300

    Add files via upload

--- funcionesVACIAS.py
+++ funcionesVACIAS.py
@@ -3,7 +3,6 @@ from configuracion import *
 import random
 import math
 
-#Lee el archivo lemario y las palabras que encuentra las agrega al diccionario
 def lectura(diccionario):
     lemario = "lemario.txt"
     with open(lemario,"r",encoding="latin-1") as archivo:
@@ -14,38 +13,38 @@ def lectura(diccionario):
 
 diccionario = []
 
-#Toma 3 listas como parámetro, según las condiciones va agregrando letras de cada lista a una cadena, mezcla las letras y retorna la cadena. 
-def dame7Letras(list1,list2,list3): 
-    cadena="" 
-    vocales=random.sample(list2, random.randint(2, 3)) 
-    cadena=cadena+"".join(vocales) 
-    
-    cantidadDeLetras=4 if len(vocales)==2 else 3 
-    letras=random.sample(list1,cantidadDeLetras) 
-    cadena=cadena+"".join(letras) 
 
-    consonanteDif=random.choice(list3)
-    cadena=cadena+"".join(consonanteDif) 
+def dame7Letras(list1,list2,list3): #tomo como parametros 3 listas
+    cadena="" #Creo una cadena vacia
+    vocales=random.sample(list2, random.randint(2, 3)) #Con random.sample selecciono una vocal de forma aleatoria, con random.randint aleatoriamente se elige entre 2 y 3 (quien determinara la cantidad de vocales que se elegirán)
+    cadena=cadena+"".join(vocales) #Agrego lo anterior a la cadena
 
-    desordenarCadena=random.sample(cadena,len(cadena)) 
-    cadenaDesordenada="".join(desordenarCadena) 
+    cantidadDeLetras=4 if len(vocales)==2 else 3 #La cantidad de letras a elegir se condiciona: serán 4 si las vocales en el paso anterior fueron 2, sino serán 3
+    letras=random.sample(list1,cantidadDeLetras) #uso random.sample para que aleatoriamente elija las letras
+    cadena=cadena+"".join(letras) #Agrego lo anterior a la cadena
 
-    return cadenaDesordenada 
+    consonanteDif=random.choice(list3) #Elijo una consonante dificil aleatoriamente con random
+    cadena=cadena+"".join(consonanteDif) #Agrego lo anterior a la cadena
+
+    desordenarCadena=random.sample(cadena,len(cadena)) #Paso como argumento la cadena y su longitud, para que random.sample la desordene aleatoriamente
+    cadenaDesordenada="".join(desordenarCadena) #Almaceno la cadena desordenada en la variable
+
+    return cadenaDesordenada #retorno la cadena desordenada
 
 letras=["b","c","d","f","g","h","j","l","m","n","p","q","r","s","t","u","v","w"]
 vocales=["a","e","i","o","u"]
 consonanteDif=["k","x","y","z"]
 
-dame7Letras(letras,vocales,consonanteDif) 
+dame7Letras(letras,vocales,consonanteDif) #llamo a la función
 
 #elige una letra de las letras en pantalla
-def dameLetra(letrasEnPantalla):
-    cadena=letrasEnPantalla 
-    letraAleatoria=random.choice(cadena) 
+def dameLetra(letrasEnPantalla): #toma el parámetro de letras en pantalla
+    cadena=letrasEnPantalla #Se pasa el valor del letras en pantalla a una variable llamada cadena
+    letraAleatoria=random.choice(cadena) #Se utiliza random choice para elegir una letra aleatoria de la cadena
 
-    return letraAleatoria 
+    return letraAleatoria #retorna la letra aleatoria elegida anteriormente
 
-#Esta funcion toma a la palabra del usuario y verifica que no haya salido (si ya salió resta), luego que cumpla los codicionales para otorgar los puntos y agregarla a la lista de palabras salidas.Si no cumple los requisitos, resta ptos.
+#Esta funcion toma a la palabra del usuario y verifica que no haya salido, luego que cumpla los codicionales para otorgar los puntos y agregarla a la lista de yaSalieron.Si no cumple los requisitos, resta ptos.
 palabrasSalidas=[]
 def procesar(letraPrincipal, letrasEnPantalla, candidata, diccionario):
     if candidata in palabrasSalidas:
@@ -56,7 +55,8 @@ def procesar(letraPrincipal, letrasEnPantalla, candidata, diccionario):
     else:
         return -1
 
-#chequea que se use la letra principal, solo use letras de la pantalla y exista en el diccionario
+#chequea que se use la letra principal, solo use letras de la pantalla y
+#exista en el diccionario
 def esValida(letraPrincipal, letrasEnPantalla, candidata, diccionario):
     if letraPrincipal in candidata:
         for letra in candidata:
@@ -68,6 +68,7 @@ def esValida(letraPrincipal, letrasEnPantalla, candidata, diccionario):
 
 #devuelve los puntos
 def puntos(candidata):
+
     if len(candidata)==3:
         return(1)
     if len(candidata)==4:
@@ -85,17 +86,7 @@ def puntos(candidata):
 #busca en el diccionario paralabras correctas y devuelve una lista de estas
 def dameAlgunasCorrectas(letraPrincipal, letrasEnPantalla, diccionario):
     algunasCorrectas = []
-    cont=0  
-    for palabra in diccionario:
-        if letraPrincipal in palabra:
-            todasCumplen=True
-            for letra in palabra:
-                if letra not in letrasEnPantalla:
-                    todasCumplen=False
-                    break
-            if todasCumplen:
-                algunasCorrectas.append(palabra)
-    return algunasCorrectas
+    cont=0  # Variable para contar las palabras encontradas
     for palabra in diccionario:
         if letraPrincipal in palabra:
             todasCumplen=True
